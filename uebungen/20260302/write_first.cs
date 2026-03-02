525db177228c66b78fa1cb1243a7a580c5180327
Lotic <239549897+LoticGit@users.noreply.github.com>
2026-03-02 15:58:19 +0100

    Notizen vom 02-03.2026 hinzugefügt.

---
+++ uebungen/20260302/write_first.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("This is a test\n");
+        Console.WriteLine("This is the first line.");
+        Console.Write("This is the ");
+        Console.Write("second line.");
+    }
+}
