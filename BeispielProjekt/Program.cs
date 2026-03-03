7ed4ad48d31e9db7e2a5d12f09b677bc500d6b4e
Andreas <foo@bar.com>
2026-03-03 16:01:42 +0100

    Beispiele zugefügt

--- BeispielProjekt/Program.cs
+++ BeispielProjekt/Program.cs
@@ -2,6 +2,36 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hallo Welt");
+        // Implizierter Cast von int -> long
+        int zahl = 3218238;
+        long große_zahl = zahl;
+
+        // Explizierter Cast von long -> int
+        // Explizit weil prinzipiell Datenverlust, so wie hier:
+        große_zahl = 2_147_483_648;
+        zahl = (int) große_zahl;
+        System.Console.WriteLine(zahl); // -2^31
+
+        // Implizierter Cast von int -> double
+        zahl = 3218238;
+        double kommazahl = zahl; // 3218238.0
+
+        // Expliziert Cast von double -> int
+        kommazahl = 3.5;
+        zahl = (int) kommazahl;
+        Console.WriteLine(zahl);
+
+        Console.WriteLine(Convert.ToInt32("f10101", 16));
+
+        // Ganzzahlige Division (links und rechts vom / ist ganzzahlig)
+        var quotient = 7 / 2;
+        Console.WriteLine(quotient);
+
+        // Gebrochenzahlige Division (links oder rechts vom / ist gebrochen)
+        quotient = 7 / 2;
+        Console.WriteLine(quotient);
     }
 }
+
+// 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 
+// 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 
