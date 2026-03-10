2180055c36205066f35cf41a3f6c4bda385064ce
Lotic <239549897+LoticGit@users.noreply.github.com>
2026-03-10 14:13:07 +0100

    Uebung Random Array vom 10-03-2026 hinzugefügt.

---
+++ uebungen/20260310/Arrayuebung_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] randomNumbers = new int[30];
+
+for(int i=0; i<randomNumbers.Length; i+=1){
+    Random random = new Random();
+    randomNumbers[i] = random.Next(1, 101);
+}
+
+foreach(int number in randomNumbers){
+    Console.WriteLine(number);
+}
