8899a4d871bc101a3c28fe6d438f634374539d11
Lotic <239549897+LoticGit@users.noreply.github.com>
2026-03-18 14:15:31 +0100

    mehr autoquatsch

--- uebungen/20260317/autos/Autoquatsch/Program.cs
+++ uebungen/20260317/autos/Autoquatsch/Program.cs
@@ -1,6 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 var auto1 = new Auto();
+auto1.Marke = "BMW";
+auto1.Baujahr = "2012";
+auto1.DisplayInfo();
+
+var auto2 = new Auto();
+auto2.Marke = "Porsche"; // Falsche Marke
+auto2.Baujahr = "2012";
+auto2.DisplayInfo();
+
+var auto3 = new Auto();
+auto3.Marke = "Trabant"; 
+auto3.Modell = "P50"; 
+auto3.Baujahr = "2012";
+auto3.DisplayInfo();
+
+var auto4 = new Auto();
+auto4.Marke = "Trabant"; 
+auto4.Modell = "p950"; // falsches Modell
+auto4.Baujahr = "2012";
+auto4.DisplayInfo();
+
+
+/*
+var auto1 = new Auto();
 auto1.Marke = "Nissan";
 auto1.Modell = "Micra";
 auto1.Baujahr = "2012";
@@ -17,6 +41,8 @@ auto3.Marke = "Koenigsegg";
 auto3.Modell = "CC850";
 auto3.Baujahr = "2024";
 auto3.DisplayInfo();
+*/
+
 /*
 var auto4 = new AutoMitKonstruktor("General Motors", "K.I.T.T", "1982");
 auto4.DisplayInfo();
@@ -30,12 +56,29 @@ class Auto {
     public string Marke {
         get{return _marke;}
         set{
+            string[] validChoices = ["BMW", "Opel", "Trabant"];
+            if(!validChoices.Contains(value))return;
             _marke = value;
+            var random = new Random();
+            List<List<string>> validModels = [["3er", "5er", "7er"], ["Corsa", "Astra", "Adam"], ["P50", "P60", "P601", "1.1"]];
+            int markeIndex = Array.IndexOf(validChoices, _marke);
+            //Console.WriteLine(markeIndex);
+            //Console.WriteLine(validModels[markeIndex].Count);
+            string randomModell = validModels[markeIndex][random.Next(0, validModels[markeIndex].Count)];
+            _modell = randomModell;
         }
     }
+
     public string Modell {
         get{return _modell;}
-        set{_modell = value;}
+        set{
+            if(_modell == null)return;
+            string[] validModell = ["BMW", "Opel", "Trabant"];
+            int markeIndex = Array.IndexOf(validModell, _marke);
+            List<List<string>> validModels = [["3er", "5er", "7er"], ["Corsa", "Astra", "Adam"], ["P50", "P60", "P601", "1.1"]];
+            if(!validModels[markeIndex].Contains(value))return;
+            _modell = value;
+        }
     }
 
     public string Baujahr {
